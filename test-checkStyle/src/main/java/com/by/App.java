d177998a1893de42051f1b0e96808795266cdbfc
baoyang3 <baoyang@jd.com>
2017-06-12 18:45:14 +0800

    checkStyle

---
+++ test-checkStyle/src/main/java/com/by/App.java
@@ -0,0 +1,17 @@
+package com.by;
+
+/**
+ * Hello world!
+ */
+public final class App {
+
+    private App() {
+    }
+
+    /**
+     * @param args asf
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
